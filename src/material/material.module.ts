d6707b5ec3c15c22da23be129c190bdf97bd97f0
Gerhardt Datsomor <datsomorg@gmail.com>
2025-05-09 14:05:14 +0000

    material ui

---
+++ src/material/material.module.ts
@@ -0,0 +1,84 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRippleModule,
+    MatTabsModule,
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatSlideToggleModule,
+    MatDividerModule,
+    MatRadioModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatListModule
+  ],
+  exports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRippleModule,
+    MatTabsModule,
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatSlideToggleModule,
+    MatDividerModule,
+    MatRadioModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
